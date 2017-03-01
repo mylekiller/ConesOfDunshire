a6e70e06b44a6524c4493d0917350c494610123a
Christopher Clarizio <christopherclarizio@gmail.com>
2017-03-01 16:18:46 -0500

    commented piece.h, commented piece.cpp

--- piece.cpp
+++ piece.cpp
@@ -1,37 +1,45 @@
 #include "piece.h"
 
-piece::piece(int x, int y,bool team){
+//defautl constructor
+piece::piece(int x, int y, bool team)
+{
 	this -> x = x;
 	this -> y = y;
 	this -> team = team;
-
 }
 
+//destructor
 piece::~piece()
 {
-	for(int i = 0; i<moves.size();i++)
+	for(int i = 0; i<moves.size();i++)   //iterates through the vector of moves
 	{
-		delete [] moves[i];
+		delete [] moves[i];   //deletes each array of integers
 	}
 }
 
+//getter function; reutnrs pointer to the vecotor of possible moves "moves"
 std::vector<int*>* piece::getMoves()
 {
 	return &moves;
 }
+
+//getter function; returns the current x position
 int piece::getX()
 {
 	return x;
 }
+
+//getter function; returns the current y position
 int piece::getY()
 {
 	return y;
 }
 
+//adds the move to the space (x,y) to the vector of possible moves "moves"
 void piece::addMove(int x, int y)
 {
-	int* move = new int[2];
-	move[0] = x;
-	move[1] = y;
-	moves.push_back(move);
+	int* move = new int[2];   //creates new pointer based integer array
+	move[0] = x;   //first element contains x position of possible move
+	move[1] = y;   //second element contains y position of possible move
+	moves.push_back(move);   //adds the new pointer based array to the vector of possible moves "moves"
 }
--- piece.h
+++ piece.h
@@ -6,26 +6,21 @@
 #define	PIECE_H
 
 class piece{
-public:
-	piece(int , int,bool);
-	~piece();
-	virtual void setMoves() = 0;
-	std::vector<int*>* getMoves();
-	int getX();
-	int getY();
-	bool getTeam();
-
-
-protected:	
-	std::vector<int*> moves;
-	void addMove(int , int);
-
-
-private:
-	int x;
-	int y;
-
-	bool team;
+	public:
+		piece(int, int, bool);   //defualt constructor; args: x position, y position, team (black/white)
+		~piece();   //default constructor
+		virtual void setMoves() = 0;   //setter function; sets the moves possible for the piece
+		std::vector<int*>* getMoves();   //getter function; returns pointer to vector of possible moves
+		int getX();   //getter function; gets the current x position of the piece
+		int getY();   //getter function; gets the current y position of the piece
+		bool getTeam();   //getter function; gets the team of the current piece
+	protected:
+		std::vector<int*> moves;   //vector of pointers to integers arrays; represents the possible moves
+		void addMove(int , int);   //adds the possibility of a move to a specific space
+	private:
+		int x;   //piece's current x position
+		int y;   //piece's current y position
+		bool team;   //piece's team; whether piece is white (true=white, false=black)
 };
 
 #endif
