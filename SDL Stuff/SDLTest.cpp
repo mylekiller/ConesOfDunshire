ca12a0bb540019be3775b45ebf4f6779a81d69dc
Kyle Miller <kmille42@nd.edu>
2017-03-20 12:09:47 -0400

    Added Some Interesting Grpahics nonsense

---
+++ SDL Stuff/SDLTest.cpp
@@ -0,0 +1,41 @@
+#include <SDL2/SDL.h>
+/*#include <SDL2/SDL_image.h>*/
+#include <stdio.h>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
+int main(int argc, char* argv[]) {
+	SDL_Window* window = NULL;
+	SDL_Surface* screenSurface = NULL;
+	if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) { 
+        printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    }
+    else {
+        //Create window
+        window = SDL_CreateWindow( "SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+        if( window == NULL ) {
+            printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+        }
+        else {
+        	//Get window surface
+            screenSurface = SDL_GetWindowSurface( window );
+
+            //Fill the surface white
+            SDL_FillRect( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );
+            
+            //Update the surface
+            SDL_UpdateWindowSurface( window );
+
+            //Wait two seconds
+            SDL_Delay( 2000 );
+        }
+    }
+    //Destroy window
+    SDL_DestroyWindow( window );
+
+    //Quit SDL subsystems
+    SDL_Quit();
+
+    return 0;
+}
