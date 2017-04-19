7f0a258d9dbeca3a23ae0dce13978a97fda8bf83
mcavanag <mcavanag@nd.edu>
2017-04-19 14:04:15 -0400

    Updated comments

--- rook.h
+++ rook.h
@@ -4,15 +4,15 @@
 #ifndef ROOK_H
 #define ROOK_H
 
-
+//Header file for the rook class, modeled after all the different piece classes, inherits from the overall piece class
 class rook:public piece
 {
         public:
-                rook(int, int, bool);   //default constructor; args: x postion, y position, team
+                rook(int, int, bool);   //default constructor with arguments for the x, y positions, and attacks
                 ~rook();   //destructor
                 void setMoves(board* bin);
-                std::vector<int*>* getAttacks();
-                void print();
+                std::vector<int*>* getAttacks(); //gets vector of attack moves
+                void print(); 
         private:
 
 };
