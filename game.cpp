cfc785d71eeaef7ce2073826f45ed629fa23016a
MacKenzie Jayne Cavanagh <mcavanag@student02.cse.nd.edu>
2017-04-19 15:28:31 -0400

    Fixed naming issue

--- game.cpp
+++ game.cpp
@@ -8,30 +8,30 @@
 game::game()
 {
 	turn = true;
-	board.addPiece(new rook(0,0,true));
-	board.addPiece(new knight(1,0,true));
-	board.addPiece(new bishop(2,0,true));
-	board.addPiece(new queen(3,0,true));
-	board.addPiece(new king(4,0,true));
-	board.addPiece(new bishop(5,0,true));
-	board.addPiece(new knight(6,0,true));
-	board.addPiece(new rook(7,0,true));
+	boardGame.addPiece(new rook(0,0,true));
+	boardGame.addPiece(new knight(1,0,true));
+	boardGame.addPiece(new bishop(2,0,true));
+	boardGame.addPiece(new queen(3,0,true));
+	boardGame.addPiece(new king(4,0,true));
+	boardGame.addPiece(new bishop(5,0,true));
+	boardGame.addPiece(new knight(6,0,true));
+	boardGame.addPiece(new rook(7,0,true));
 	for(int i = 0;i<8;i++)
 	{
-		board.addPiece(new pawn(i,1,true));
+		boardGame.addPiece(new pawn(i,1,true));
 	}
 
-	board.addPiece(new rook(0,7,true));
-	board.addPiece(new knight(1,7,true));
-	board.addPiece(new bishop(2,7,true));
-	board.addPiece(new queen(3,7,true));
-	board.addPiece(new king(4,7,true));
-	board.addPiece(new bishop(5,7,true));
-	board.addPiece(new knight(6,7,true));
-	board.addPiece(new rook(7,7,true));
+	boardGame.addPiece(new rook(0,7,true));
+	boardGame.addPiece(new knight(1,7,true));
+	boardGame.addPiece(new bishop(2,7,true));
+	boardGame.addPiece(new queen(3,7,true));
+	boardGame.addPiece(new king(4,7,true));
+	boardGame.addPiece(new bishop(5,7,true));
+	boardGame.addPiece(new knight(6,7,true));
+	boardGame.addPiece(new rook(7,7,true));
 	for(int i = 0;i<8;i++)
 	{
-		board.addPiece(new pawn(i,6,true));
+		boardGame.addPiece(new pawn(i,6,true));
 	}
 
 }
@@ -39,5 +39,5 @@ game::~game(){}
 
 void game::print()
 {
-	board.printBoard();
+	boardGame.printBoard();
 }
