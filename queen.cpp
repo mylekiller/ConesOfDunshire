cf9955a72377c044eb140f3e72fbd2fdf0964f2e
MacKenzie Jayne Cavanagh <mcavanag@student02.cse.nd.edu>
2017-04-19 14:15:21 -0400

    Added comments to the queen piece

--- queen.cpp
+++ queen.cpp
@@ -1,4 +1,4 @@
-
+//Queen implementation file that allows the queen to move left, right, up, down and diagonal any number of spaces. 
 #include "queen.h"
 #include "board.h"
 void queen::setMoves(board* bin)
@@ -9,10 +9,10 @@ void queen::setMoves(board* bin)
         {
                 int x,y; 
                 for (int dis = 1; dis <= 7; dis++) {
-                        x = getX() + xdir*dis;
+                        x = getX() + xdir*dis; //allows the piece to move to the right
                         y = getY();
                         if(inbounds(x,y) && bin -> isOccupied(x,y))
-                        {
+                        { //if inbounds, adds move
                                 if(bin -> getTeam(x,y) != getTeam())
                                 {
                                         addMove(x,y);
@@ -35,11 +35,11 @@ void queen::setMoves(board* bin)
                 int x,y;  
                 for (int dis = 1; dis <= 7; dis++) {
                         x = getX();
-                        y = getY() + ydir*dis;
+                        y = getY() + ydir*dis; //allows the piece to move up or down
 
 
                         if(inbounds(x,y) && bin -> isOccupied(x,y))
-                        {
+                        { //checks to ensure the move is in the bounds of the board and not moving through other pieces
                                 if(bin -> getTeam(x,y) != getTeam())
                                 {
                                         addMove(x,y);
@@ -67,7 +67,7 @@ void queen::setMoves(board* bin)
                                 int x;
                                 int y;
                                
-                                x = getX() + xdir*dis;
+                                x = getX() + xdir*dis; //allows the piece to move diagonally
                                 y = getY() + ydir*dis;
 
 
@@ -93,7 +93,7 @@ void queen::setMoves(board* bin)
         
 }
 
-void queen::print()
+void queen::print() //prints the queen on the board, q or Q depending on black or white pieces
 {
         if(getTeam())
                 std::cout<<'q';
