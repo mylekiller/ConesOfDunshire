e9a2b8263c6baae184d47ece7b585aa12abff259
Christopher Clarizio <christopherclarizio@gmail.com>
2017-04-23 15:31:25 -0400

    finished ai (i think); time to test it

--- AI.cpp
+++ AI.cpp
@@ -13,33 +13,70 @@ AI::~AI()
 
 pair<pair<int,int>,pair<int,int>> AI::minimax(board currentBoard, bool team, int depth)
 {
-	int i,j;
-	int bestValue = -INT_MAX;
-	pair<pair<int,int>,pair<int,int>> bestMove;
+	int bestValue = INT_MAX;    //placeholder value, should be exceeded by anything
+	if(team)    //if AI is playing for white
+	{
+		bestValue = -INT_MAX;    //starting best value is large negative number
+								 //white is seeking to maximize values (most positive)
+	}
+	else    //if AI is playing for black
+	{
+		bestValue = INT_MAX;    //starting best value is large positive number
+								//black is seeking to minimize values (most negative)
+	}
 
-	for(int i = 0; i < 8; ++i)
+	pair<pair<int,int>,pair<int,int>> bestMove;    //tracks the best move that been found
+
+	for(int i = 0; i < 8; ++i)    //iterates through the 8x8
 	{
 		for(int j = 0; j < 8; ++j)
 		{
+			//if the current space occupied and the piece is the color the AI is playing
 			if(currentBoard.isOccupied(i,j) && currentBoard.get(i,j)->getTeam() == team)
 			{
+				//list of moves piece in current sqaure can make
 				auto moves = currentBoard.get(i,j)->getMoves();
-				for(auto it = moves.begin(); it != moves.end(); ++it)
+				//iterate through all the moves the piece in current sqaure can make
+				for(auto it = moves->begin(); it != moves->end(); ++it)
 				{
-				
-				}
-			}
-			else if(currentBoard.isOccupied(i,j) && currentBoard.get(i,j)->getTeam != team)
-			{
-				auto moves = currentBoard.get(i,j)->getMoves();
-				for(auto it = moves.begin(); it != moves.end(); ++it)
-				{
-				
+
+					board newBoard(currentBoard);    //newBoard is where each move is tried
+					if(!newBoard.isAllowed(newBoard.get(i,j), (*it)[0], (*it)[1], team))
+					{
+						continue;
+					}
+					newBoard.execmove(newBoard.get(i,j), (*it)[0], (*it)[1]);
+
+					int value;
+					if(team)    //fi AI is playing for white
+					{
+						value = maxSearch(newBoard, depth-1);
+						if(value > bestValue)
+						{
+							bestValue = 			 value;
+							bestMove.first.first = 	 i;
+							bestMove.first.second =  j;
+							bestMove.second.first =  (*it)[0];
+							bestMove.second.second = (*it)[1];
+						}
+					}
+					else    //if AI is playing for black
+					{
+						value = minSearch(newBoard, depth-1);
+						if(value < bestValue)
+						{
+							bestValue = 			 value;
+							bestMove.first.first =	 i;
+							bestMove.first.second =  j;
+							bestMove.second.first =  (*it)[0];
+							bestMove.second.second = (*it)[1];
+						}
+					}
 				}
 			}
 		}
 	}
-
+	return bestMove;
 }
 
 int AI::minSearch(board currentBoard, int depth)
@@ -54,13 +91,24 @@ int AI::minSearch(board currentBoard, int depth)
 	{
 		for(int j = 0;  j < 8; ++j)
 		{
-			auto moves = currentBoard.get(i,j)->getMoves();
-			for(auto it = moves.begin(); it != moves.end(); ++it)
+			if(currentBoard.isOccupied() && !currentBoard.get(i,j)->getTeam())
 			{
-				//copy board into new board
-				//make the move
-				//set value to maxsearch on depth-1 and new board
-				//if value < bestValue set bestValue to value
+				auto moves = currentBoard.get(i,j)->getMoves();
+				for(auto it = moves->begin(); it != moves->end(); ++it)
+				{
+					board newBoard(currentBoard);
+					if(!newBoard.isAllowed(newBoard.get(i,j), (*it)[0], (*it)[1], false)
+					{
+						continue;
+					}
+					newBoard.execmove(newBoard.get(i,j), (*it)[0], (*it)[1]);
+
+					int value = maxSearch(newBoard, depth-1);
+					if(value < bestValue )
+					{
+						bestValue = value;
+					}
+				}
 			}
 		}
 	}
@@ -79,13 +127,28 @@ int AI::maxSearch(board currentBoard, int depth)
 	{
 		for(int j = 0; j < 8; ++j)
 		{
-			auto moves = currentBoard.get(i,j)->getMoves();
-			for(auto it - moves.begin(); it != moves.end(); ++it)
+			if(currentBoard.isOccupied() && currentBoard.get(i,j)->getTeam())
 			{
-				//WIP
+				auto moves = currentBoard.get(i,j)->getMoves();
+				for(auto it = moves->begin(); it != moves->end(); ++it)
+				{
+					board newBoard(currentBoard);
+					if(!newBoard.isAllowed(newBoard.get(i,j), (*it)[0], (*it)[1], true))
+					{
+						continue;
+					}
+					newBoard.execmove(newBoard.get(i,j), (*it)[0], (*it)[1]);
+
+					int value = minSearch(newBoard, depth-1);
+					if(value > bestValue)
+					{
+						bestValue = value;
+					}
+				}
 			}
 		}
 	}
+	return bestValue;
 }
 
 int AI::evaluate(board currentBoard)
