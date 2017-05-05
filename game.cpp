37dac83cbfa1173b6c8988410f70108faaf482b8
mcavanag <mcavanag@nd.edu>
2017-05-05 09:38:24 -0400

    Commented

--- game.cpp
+++ game.cpp
@@ -5,8 +5,9 @@
 #include "bishop.h"
 #include "knight.h"
 #include "king.h"
+//Implementation of the game class
 game::game()
-{
+{ //creates a game and sets up all the pieces on the board (black and white) in the correct positions
 	turn = true;
 	boardGame.addPiece(new rook(0,0,true));
 	boardGame.addPiece(new knight(1,0,true));
@@ -16,7 +17,7 @@ game::game()
 	boardGame.addPiece(new bishop(5,0,true));
 	boardGame.addPiece(new knight(6,0,true));
 	boardGame.addPiece(new rook(7,0,true));
-	for(int i = 0;i<8;i++)
+	for(int i = 0;i<8;i++) //adding the 8 pawns
 	{
 		boardGame.addPiece(new pawn(i,1,true));
 	}
@@ -40,26 +41,26 @@ game::game()
 	boardGame.update(true);
 
 }
-game::~game(){}
+game::~game(){} //deconstructor
 
 piece* game::getpiece(int x,int y)
 {
 	return boardGame.get(x,y);
 }
-void game::print()
+void game::print() //prints the board
 {
 	boardGame.printBoard();
 }
-bool game::getTurn()
+bool game::getTurn() //gets which player's turn it is
 {
 	return turn;
 }
 bool game::trymove(int sx,int sy, int tx, int ty, enum piecetype ptype)
 {
-	if(boardGame.isOccupied(sx,sy) && boardGame.getTeam(sx,sy) == turn)
+	if(boardGame.isOccupied(sx,sy) && boardGame.getTeam(sx,sy) == turn) //checks if occupied
 	{
 
-		if(boardGame.isAllowed(boardGame.get(sx,sy), tx, ty, turn))
+		if(boardGame.isAllowed(boardGame.get(sx,sy), tx, ty, turn)) //checks if move is allowed
 		{
 			boardGame.execmove(boardGame.get(sx,sy),tx,ty ,ptype);
 			turn = !turn;
@@ -90,15 +91,15 @@ bool game::trymove(int sx,int sy, int tx, int ty, enum piecetype ptype)
 	return false;
 }
 
-bool game::checkPromotion(int fx,int fy, int tx, int ty)
+bool game::checkPromotion(int fx,int fy, int tx, int ty) //checks for if promotion is possible
 {
 	return boardGame.isAllowed(boardGame.get(fx,fy),tx,ty,turn);
 }
-int game::inCheck(bool team) {
+int game::inCheck(bool team) { //checks to see if the King is in check
 	return boardGame.check(team);
 }
 
-std::pair<std::pair<int,int> , std::pair<int,int> > game::getAIMove()
+std::pair<std::pair<int,int> , std::pair<int,int> > game::getAIMove() 
 {
 	return ai.iterate(boardGame, turn, 3).m;
 }
