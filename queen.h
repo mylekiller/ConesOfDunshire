145c05d9d271ea50e90131b300d1ed5aa362adf3
mcavanag <mcavanag@nd.edu>
2017-05-05 08:53:34 -0400

    Commented

--- queen.h
+++ queen.h
@@ -2,15 +2,15 @@
 #ifndef QUEEN_H
 #define QUEEN_H
 
-
+//Header file for the queen class, modeled after all the different piece classes, inherits from the overall piece class
 class queen:public piece
 {
         public:
                 queen(int, int, bool);   //default constructor; args: x postion, y position, team
                 queen(const queen&);
                 ~queen();   //destructor
-                void setMoves(board* bin);
-                void setAttacks(board*);
+                void setMoves(board* bin); //fils vector of "moves" with all possible moves
+                void setAttacks(board*); //gets possible attacks
                 void print();
 
                bool operator==(const piece& pin) const;
