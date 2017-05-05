21d9f58956f82bcea0dc6adaedea327781079a66
mcavanag <mcavanag@nd.edu>
2017-05-05 08:55:21 -0400

    Commented

--- pawn.h
+++ pawn.h
@@ -3,7 +3,7 @@
 #ifndef PAWN_H
 #define PAWN_H
 
-
+//Header file for the pawn class, modeled after all the different piece classes, inherits from the overall piece class
 class pawn:public piece
 {
 	public:
@@ -11,18 +11,18 @@ class pawn:public piece
 		pawn(const pawn&);
 		~pawn();   //destructor
 
-		void print();
-		void setMoves(board*);
-		void setAttacks(board*);
+		void print(); //prints board
+		void setMoves(board*);  //fils vector of "moves" with all possible moves
+		void setAttacks(board*); //gets vector of possible attacks
 		void resetMoves();
-		bool movedTwo();
-		void setMovedTwo(bool);
+		bool movedTwo();  
+		void setMovedTwo(bool);//allows pawns the ability to move two spots
 
 		bool operator==(const piece& pin) const;
 
 
 	private:
-		bool movedtwo;
+		bool movedtwo; //true if pawn moved two spots
 };
 
 #endif
