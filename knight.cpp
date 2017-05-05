47117f0a0d090510dc5f5b4cd9c1b019c8615fcc
mcavanag <mcavanag@nd.edu>
2017-05-05 09:05:05 -0400

    Commented

--- knight.cpp
+++ knight.cpp
@@ -1,13 +1,13 @@
  #include "board.h"
  #include "knight.h"
-
+//Implementation of Knight class, includes functions that get all possible moves for the rook (based on how to piece is allowed to move following the conventional rules of chess)
 void knight::setMoves(board* bin)
 {
 	for(int xdir = -1;xdir<=1;xdir+=2)
 	{
 		for(int ydir = -1;ydir<=1;ydir+=2)
 		{
-			int x = getX() + xdir*2;
+			int x = getX() + xdir*2; //allows knight to move in "L" shape
 			int y = getY() + ydir;
 
 			if(inbounds(x,y) && (! bin -> isOccupied(x,y) || bin -> getTeam(x,y) != getTeam()))
@@ -16,7 +16,7 @@ void knight::setMoves(board* bin)
  			}
 
  			x = getX() + xdir;
- 			y = getY() + ydir*2;
+ 			y = getY() + ydir*2; //allows knight to move in "L" shape
 			if(inbounds(x,y) && (! bin -> isOccupied(x,y) || bin -> getTeam(x,y) != getTeam()))
  			{
  				addMove(x,y);
@@ -24,7 +24,7 @@ void knight::setMoves(board* bin)
 		}
 	}
 }
-void knight::setAttacks(board* bin)
+void knight::setAttacks(board* bin) //stores the possible pieces the knight can attack
 {
 	for(int xdir = -1;xdir<=1;xdir+=2)
 	{
@@ -59,7 +59,7 @@ bool knight::operator==(const piece& pin) const
 	return false;
 }
 
-void knight::print()
+void knight::print() //prints out the piece
 {
 	if(!getTeam())
 		std::cout<<'n';
@@ -67,7 +67,7 @@ void knight::print()
 		std::cout<<'N';
 }
 
-knight::knight(int x, int y, bool team) : piece(x,y,team)
+knight::knight(int x, int y, bool team) : piece(x,y,team) //constructor
 {
 	setType(KNIGHT);
 }
@@ -76,4 +76,4 @@ knight::knight(const knight& kin) : piece(kin.x,kin.y,kin.team)
 	setType(KNIGHT);
 	copyData((piece*)(&kin));
 }
-knight::~knight(){}
+knight::~knight(){} //deconstructor
